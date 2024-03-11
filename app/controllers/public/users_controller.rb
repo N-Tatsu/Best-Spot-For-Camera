c10b9eeac8f7ba0402fcc238aa2aa1020f2bf951
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-03-11 09:29:00 +0000

    [add]user_edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,24 +1,46 @@
 class Public::UsersController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit,:update]
+
   def index
-    @users = User.all 
+    @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images
-  
+
   end
 
   def edit
+    @user = User.find(params[:id])
   end
-  
+
   def update
+    if @user.update(user_params)
+       redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
   end
 
   def unsubscribe
   end
-  
+
   def withdraw
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
