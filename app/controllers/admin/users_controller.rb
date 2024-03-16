d1248faa1693a4f1c99f63205480dac771fa6205
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-03-16 13:07:03 +0000

    [add]admin/user_show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,8 +4,11 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
   end
 
   def edit
+    @user = user.find(params[:id])
   end
 end
