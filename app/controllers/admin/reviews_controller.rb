b140a3d164426a9692933fe28a73a5e9931e3bb1
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-03-17 02:32:07 +0000

    [add]admin/reviews

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,7 +1,12 @@
 class Admin::ReviewsController < ApplicationController
   def index
+    @post_comments = PostComment.all
+    @users = User.all
   end
 
   def destroy
+    @post_comment = PostComment.find(params[:id])
+    @post_comment.destroy
+    redirect_to admin_reviews_path
   end
 end
