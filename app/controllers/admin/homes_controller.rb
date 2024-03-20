5a739b29a2a430740554af0bc4de51c03c8d69b7
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-03-20 22:53:43 +0000

    [fix/layouts]admin_post.show

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,9 +1,9 @@
 class Admin::HomesController < ApplicationController
-  
+
   before_action :authenticate_admin!
-  
+
   def top
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).per(10)
   end
-  
+
 end
