df642e7a328d8189bd4f2279b293b87cabe38270
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-03-23 06:41:17 +0000

    [fix]error_message.japanese

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -7,10 +7,12 @@ class Public::PostImagesController < ApplicationController
   end
 
   def create
-      if current_user.guest_user?
-          redirect_to post_images_path, alert: "ゲストログインユーザーは投稿できません"
-          return
-      end
+      #ゲストログインユーザーの場合は投稿できない（今回は投稿できるようにコメントアウト）
+              # if current_user.guest_user?
+              #     redirect_to post_images_path, alert: "ゲストログインユーザーは投稿できません"
+              #     return
+              # end
+      
       #byebug
       @post_image = PostImage.new(post_image_params)
       @post_image.user_id = current_user.id
