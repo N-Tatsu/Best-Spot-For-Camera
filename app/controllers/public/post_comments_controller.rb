74cc5c1f7dea096fc2a882788d9a63e9723d34d6
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-03-24 06:27:28 +0000

    [fix]public_post_comment.flash_message

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,13 +1,16 @@
 class Public::PostCommentsController < ApplicationController
-  
+
   before_action :authenticate_user!
 
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
-    comment.save
-    redirect_to request.referer
+    if  comment.save
+        redirect_to request.referer, notice: "コメントを保存しました"
+    else
+        redirect_to request.referer, alert: "※コメントの保存に失敗しました"
+    end
   end
 
   def destroy
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,4 +1,7 @@
 class PostComment < ApplicationRecord
+
+  validates :comment, length: { maximum: 50 }
+
   belongs_to :user
   belongs_to :post_image
 end
