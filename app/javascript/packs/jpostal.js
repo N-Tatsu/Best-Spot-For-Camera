4bee5cca2495f8009f512334e3ef6770908af7f8
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-03-24 09:28:29 +0000

    [fix]map

--- app/javascript/packs/jpostal.js
+++ app/javascript/packs/jpostal.js
@@ -1,10 +1,8 @@
 import "jquery-jpostal-ja"
 // 郵便番号を入力すると自動で住所に変換してくれる
-document.addEventListener('turbolinks:load', function() {
-  $('#zipcode').jpostal({
-    postcode : ['#zipcode'],
-    address : {
-      '#post_image_address': '%3%4%5'
-    }
-  });
-}, false)
+$('#zipcode').jpostal({
+  postcode : ['#zipcode'],
+  address : {
+    '#post_image_address': '%3%4%5'
+  }
+});
