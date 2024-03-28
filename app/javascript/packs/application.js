7366ec3d92c22ec2e337020730b546db48d30750
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-03-28 10:06:34 +0000

    [fix]top_buttun

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
+import "script.js";
 window.$ = window.jQuery = require("jquery");
 
 Rails.start()
